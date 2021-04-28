7b0394d72ce9246cd049db4dc16e0eaa93fe4734
cperezimbert <72502444+cperezimbert@users.noreply.github.com>
2021-04-27 21:44:07 -0300

    changes done to index, style and script.

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    if(checkdata()){
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    }else{
+        return false;
+      }
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+function checkdata(){
+            //Create references to the input elements we wish to validate
+           var principal = document.getElementById("principal");
+            
+           //Check if field is empty
+           if(principal.value == ""){
+             alert("Please enter the amount");
+             principal.focus();
+              return false;
+             }
+          //Check if field is a positive number
+           if(principal.value <0){
+              alert("Please enter a positive number");
+             principal.focus();
+              return false;
+             }
+         //If all is well return true.
+             return true;
+          }       
